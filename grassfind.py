84b84091c60bd11751632f96f662f0bf603af5f4
Kylianalex <66625058+Kylianalex@users.noreply.github.com>
2021-10-02 22:05:19 +0200

    make an update (i don't know)

--- grassfind.py
+++ grassfind.py
@@ -1,33 +1,63 @@
 #!/usr/bin/env python
 import jnbt, os
 
+def create_file(path):
+    world = jnbt.World(path)
+    overworld = world[jnbt.DIM_OVERWORLD]
+    f = open(os.path.basename(os.path.normpath(path)) + ".txt", "w")
+    f.write(path + "\n")
+    i = 0
+    for block in overworld.iterBlocks():
+        #print(block.getID())
+        if block.getID() == 31:
+            f.write(str(block.x) + " " + str(block.y) + " " + str(block.z) + "\n")
+            i += 1
+    return i
+
+def verify_file(path):
+    f_ = open(path, "r")
+    f = f_.read().split("\n")
+    path = f[0]
+    f_.close()
+    world = jnbt.World(path)
+    overworld = world[jnbt.DIM_OVERWORLD]
+    blocks = f[1::]
+    f = open(os.path.basename(os.path.normpath(path)) + ".txt", "w")
+    f.write(path + "\n")
+    i = 0
+    for block in blocks:
+        if block:
+            x, y, z = block.split(" ")
+            b = overworld.getBlock(int(x), int(y), int(z))
+            if b.getID() == 31:
+                f.write(x + " " + y + " " + z + "\n")
+                i += 1
+    return len(blocks) - 1, i
+
 if __name__ == "__main__":
-    choice = input ("1 | generate file\n2 | check a generated file\nchoice : ")
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Find grass in a minecraft world.')
+    parser.add_argument('-o', '--option', choices=['create','verify'], help="Choose the option")
+    parser.add_argument('-f', '--file', help="Choose the folder of the map or the file to be verified")
+    args = parser.parse_args()
+    choice = False
+    path = False
+    if args.option == "create":
+        choice = "1"
+    elif args.option == "verify":
+        choice = "2"
+    if args.file:
+        path = args.file
+    if not choice:
+        choice = input("1 | generate file\n2 | check a generated file\nchoice : ")
     if choice == "1":
-        path = input("save path : ")
-        world = jnbt.World(path)
-        overworld = world[jnbt.DIM_OVERWORLD]
-        f = open(os.path.basename(os.path.normpath(path)) + ".txt", "w")
-        f.write(path + "\n")
-        for block in overworld.iterBlocks():
-            #print(block.getID())
-            if block.getID() == 31:
-                f.write(str(block.x) + " " + str(block.y) + " " + str(block.z) + "\n")
-        print("the coordinate of all grass blocks are in " + os.path.basename(os.path.normpath(path)) + ".txt")
+        if not path:
+            path = input("save path : ")
+        num = create_file(path)
+        print("found", num, "grass")
     elif choice == "2":
-        path = input("generated file path : ")
-        f_ = open(path, "r")
-        f = f_.read().split("\n")
-        f_.close()
-        world = jnbt.World(f[0])
-        overworld = world[jnbt.DIM_OVERWORLD]
-        blocks = f[1::]
-        f = open(os.path.basename(os.path.normpath(path)), "w")
-        f.write(path + "\n")
-        for block in blocks:
-            if block:
-                x, y, z = block.split(" ")
-                b = overworld.getBlock(int(x), int(y), int(z))
-                if b.getID() == 31:
-                    f.write(x + " " + y + " " + z + "\n")
-        print("the coordinate of all grass blocks are in " + os.path.basename(os.path.normpath(path)))
+        if not path:
+            path = input("generated file path : ")
+        oldnum, newnum = verify_file(path)
+        print("found", newnum, "grass.", (oldnum - newnum), "grass removed from file")
